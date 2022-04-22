a83aacf96909a041eb494bd5fbb6d8b21cd5aef6
Tgillett84 <tamekatlc2011@gmail.com>
2022-04-22 13:34:13 +0900

    update made

--- Data Metrics T/Updated handsOn.R
+++ Data Metrics T/Updated handsOn.R
@@ -4,7 +4,7 @@ library("dplyr")
 library("rcompanion")
 library("car")
 
-##Does the average price of avocados differ between Albany, 
+##Does the average price of avocados differ between Albany
 ##Houston, and Seattle? Need AveragePrice and region
 
 head(avocados)
@@ -13,10 +13,10 @@ head(avocados)
 
 table(avocados$region)
 
-avo1 <- na.omit(avocados %>% filter(region %in% c("Albany", "Houston", "Seattle")))
+avo1 <- na.omit(avocados %>% filter(region %in% c("Albany", "Houston", "Seattle"))) # nolint
 table(avo1$region)
 
-avo1$AveragePrice <- as.numeric(avo1$AveragePrice)
+avo1$AveragePrice <- as.numeric(avo1$AveragePrice) # nolint
 
 ##Determine Distribution
 
@@ -24,31 +24,31 @@ plotNormalHistogram(avo1$AveragePrice)
 
 ##Only slightly postively skewed will try SQRT
 
-avo1$AveragePriceSQRT <- sqrt(avo1$AveragePrice)
+avo1$AveragePriceSQRT <- sqrt(avo1$AveragePrice) # nolint
 plotNormalHistogram(avo1$AveragePriceSQRT)
 
-avo1$AveragePriceLOG <- log(avo1$AveragePrice)
+avo1$AveragePriceLOG <- log(avo1$AveragePrice) # nolint
 plotNormalHistogram(avo1$AveragePriceLOG)
 
 ##Log looks the best but SQRT is less drastic a change
 
 ##Test
 
-bartlett.test(AveragePriceSQRT ~ region, data=avo1)
+bartlett.test(AveragePriceSQRT ~ region, data=avo1) # nolint
 
-## P significant 
-fligner.test(AveragePriceSQRT ~ region, data=avo1)
+## P significant  # nolint
+fligner.test(AveragePriceSQRT ~ region, data=avo1) # nolint
 
 ##P still significant
 
-ANOVA <- lm(AveragePriceSQRT ~ region, data=avo1)
-Anova(ANOVA, Type="II", white.adjust=TRUE)
+ANOVA <- lm(AveragePriceSQRT ~ region, data=avo1) # nolint
+Anova(ANOVA, Type="II", white.adjust=TRUE) # nolint
 
-##The P value is significant so we can assume region does affect price of avocados
+##The P value is significant so we can assume region does affect price of avocados # nolint
 
-pairwise.t.test(avo1$AveragePriceSQRT, avo1$region, p.adjust = "bonferroni", pool.sd = FALSE)
+pairwise.t.test(avo1$AveragePriceSQRT, avo1$region, p.adjust = "bonferroni", pool.sd = FALSE) # nolint
 
-avo1Means = avo1 %>% group_by(region) %>% summarize(Mean = mean(AveragePrice))
+avo1Means = avo1 %>% group_by(region) %>% summarize(Mean = mean(AveragePrice)) # nolint
 avo1Means
 
 ##The price does vary by region. Albany has an Avg Price of 1.56, Houston 1.05
